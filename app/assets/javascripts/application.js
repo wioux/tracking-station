b9438dc63fc1012702e5b7a7cbf65f2c6948c56c
Peter Woo <petersnowdonwoo@gmail.com>
2016-03-02 21:07:42 -0800

    Create object for making Body out of our json format

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,40 +5,9 @@
 //= require_tree ./application
 
 $(document).ready(function() {
-  var bodies = {};
-
-  $.get('bodies.json', function(resp) {
-    resp.forEach(function(body) {
-      var object = new Body(body.name);
-      bodies[body.id] = object;
-      window[body.name] = object;
-
-      object.spacecraft = (body.classification == 'Spacecraft');
-      object.sun = (body.classification == 'Star');
-
-      object.radiusKm = body.radius_km;
-
-      if (body.inner_ring_radius_km) {
-        object.rings = new PlanetaryRing(body.inner_ring_radius_km,
-                                         body.outer_ring_radius_km);
-        object.rings.texture = "textures/"+body.ring_texture
-      }
-
-      object.npRA = body.north_pole_right_ascension || 0;
-      object.npDEC = body.north_pole_declination || 0;
-
-      object.color = body.color ? parseInt(body.color) : 0x808080;
-      if (body.texture)
-        object.texture = "textures/"+body.texture;
-
-      if (body.parent_id)
-        bodies[body.parent_id].addSatellite(object);
-
-      if (body.ephemerides)
-        object.addEphemerides(body.ephemerides);
+  new Loader({ textures: 'textures/' })
+    .loadBodies(function(bodies) {
+      var jd = Earth.ephemerides[0].jd;
+      window.sys = new SystemBrowser(document.getElementById('ui'), Sun, jd);
     });
-
-    var jd = Earth.ephemerides[0].jd;
-    var ctx = new SystemBrowser(document.getElementById('ui'), Sun, jd);
-  });
 });
