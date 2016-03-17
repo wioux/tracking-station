d64d0da6206faaa9863b52f3698e97b63db3f9c7
Peter Woo <petersnowdonwoo@gmail.com>
2016-03-16 20:04:36 -0700

    Use bodies[] query parameter to restrict system

--- app/controllers/bodies_controller.rb
+++ app/controllers/bodies_controller.rb
@@ -1,29 +1,17 @@
 class BodiesController < ApplicationController
   def index
-    @body ||= Body.find_by!(classification: 'Star')
+    @body = Body.find_by!(classification: 'Star')
+    @satellite_ids = params[:bodies] if params.key?(:bodies)
   end
 
   def show
-    @body ||= Body.find(params[:id])
+    @body = Body.find(params[:id])
+    @satellite_ids = params[:bodies] if params.key?(:bodies)
 
     respond_to do |format|
       format.html
       format.json do
-        # include info for body
-        bodies = [@body]
-
-        # include info for body's moons
-        bodies += Body.where(parent_id: @body).preload(:ephemerides, :timeline_events)
-
-        # include info for satellites of body or its moons
-        bodies += Body.joins(:ephemerides).
-                 where(ephemerides: { central_body_id: bodies }).
-                 select("bodies.*").distinct.
-                 preload(:ephemerides, :timeline_events)
-
-        bodies.uniq!
-
-        render json: bodies,
+        render json: requested_system,
                only: [:id, :parent_id, :name, :classification,
                       :radius_km, :inner_ring_radius_km, :outer_ring_radius_km,
                       :north_pole_right_ascension, :north_pole_declination,
@@ -32,5 +20,27 @@ class BodiesController < ApplicationController
                methods: [:ephemerides, :marked_up_info, :timeline_events]
       end
     end
+  end
+
+  private
+
+  def requested_system
+    # include info for body
+    bodies = [@body]
+
+    if params.key?(:bodies)
+      bodies += Body.where(id: params[:bodies]).preload(:ephemerides, :timeline_events)
+    else
+      # include info for body's moons
+      bodies += Body.where(parent_id: @body).preload(:ephemerides, :timeline_events)
+
+      # include info for satellites of body or its moons
+      bodies += Body.joins(:ephemerides).
+               where(ephemerides: { central_body_id: bodies }).
+               select("bodies.*").distinct.
+               preload(:ephemerides, :timeline_events)
+    end
+
+    bodies.uniq!
   end
 end
