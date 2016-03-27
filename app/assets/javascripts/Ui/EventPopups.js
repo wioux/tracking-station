2c526f13fb57854f309ee752d3bc34f8da750070
Peter Woo <petersnowdonwoo@gmail.com>
2016-03-27 16:58:57 -0700

    Each body has its own event popup which follows it and collates events.

--- app/assets/javascripts/Ui/EventPopups.js
+++ app/assets/javascripts/Ui/EventPopups.js
@@ -1,22 +1,43 @@
 
 EventPopups = function(system, parent) {
+  var timeouts = {}, containers = {};
+
+  var width = 200;
+
   system.eachBody(function(body) {
     body.timeline.forEach(function(event) {
       system.clock.on(event.jd, function() {
-        var popup = _('div', { class: "event-popup", parent: parent });
-        popup.textContent = event.description;
+        var popup = containers[body.id];
+        if (!popup) {
+          popup = containers[body.id] = _('div', { class: "event-popup",
+                                                   style: "width: "+width+"px",
+                                                   parent: parent });
+          _('header', { parent: popup }).textContent = body.name;
+        }
 
-        var pos3d = body.object3d.position.clone();
-        var f = system.addEventListener('update', function() {
-          var pos2d = system.project(pos3d);
-          popup.style.left = pos2d.x + 'px';
-          popup.style.top = pos2d.y + 'px';
-        });
+        if (timeouts[body.id]) {
+          clearTimeout(timeouts[body.id][1]);
+        } else {
+          var f = system.addEventListener('update', function() {
+            var pos2d = system.project(body.object3d.position);
+            popup.style.left = (pos2d.x - width/2) + 'px';
+            popup.style.top = pos2d.y + 'px';
+          });
 
-        setTimeout(function() {
-          system.removeEventListener('update', f);
-          popup.parentNode.removeChild(popup);
-        }, 6000);
+          timeouts[body.id] = [function() {
+            system.removeEventListener('update', f);
+            $(popup).find('p').remove();
+            popup.style.display = 'none';
+            timeouts[body.id] = null;
+          }];
+        }
+
+        timeouts[body.id][1] = setTimeout(timeouts[body.id][0], 6000);
+
+        _('p', { parent: popup }).textContent = event.description;
+
+        popup.style.display = 'block';
+
       });
     });
   });
