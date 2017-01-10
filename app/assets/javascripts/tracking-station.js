ec70684907f8434c92aa25ffa998eddc8b349fd1
Peter Woo <petersnowdonwoo@gmail.com>
2017-01-09 21:25:03 -0800

    Move app init/styles out of application.js/application.css

---
+++ app/assets/javascripts/tracking-station.js
@@ -0,0 +1,51 @@
+//= require three
+//= require OrbitControls
+//= require_tree ./application
+//= require_tree ./3d
+//= require_tree ./Ui
+
+$(document).ready(function() {
+  $('.tracking-station').each(function() {
+    var container = this;
+    var root = parseInt($(this).data('root'));
+    var trackingStation = this;
+
+    new AppLoader({ textures: '/textures/' })
+      .loadBodies($(this).data('href'), function(bodies) {
+        var jd = 2455794.330554163;
+
+        root = bodies.find(function(x) { return x.id == root });
+
+        window.sys = new SystemBrowser(container, bodies, root);
+        window.sys.createMilkyWay('/textures/ESO_-_Milky_Way-Cropped.jpg');
+
+        var sidebar = $(_('div', { parent: container, class: 'sidebar' }));
+        new StatePanel(sys, sidebar);
+        new BodyListPanel(sys, sidebar);
+
+        new InfoPanels(sys, container);
+        new BodyTooltip(sys, container);
+        new EventPopups(sys, container);
+
+        if (!root.sun)
+          sys.setAmbientLight(0xffffff);
+
+        if (trackingStation.dataset.focus) {
+          var focus = bodies.find(function(x) { return x.id == trackingStation.dataset.focus });
+          sys.setFocus(focus);
+        }
+
+        sys.start(jd);
+
+        bodies
+          .filter(function(body) { return body.ephemerides.href })
+          .forEach(function(body) {
+            $.get(body.ephemerides.href, function(ephemerides) {
+              ephemerides.forEach(function(eph) { eph.jd = parseFloat(eph.jd) });
+              ephemerides.sort(function(a, b) { return a.jd - b.jd });
+              body.ephemerides.addAll(ephemerides);
+            });
+          });
+      });
+  });
+});
