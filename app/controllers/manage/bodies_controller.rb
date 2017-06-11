db2025dafeadb092243aa5232acf787e3a0864f1
Peter Woo <petersnowdonwoo@gmail.com>
2017-06-11 09:51:36 -0700

    Tweak some json results

--- app/controllers/manage/bodies_controller.rb
+++ app/controllers/manage/bodies_controller.rb
@@ -19,9 +19,20 @@ module Manage
     private
 
     def set_bodies_results
-      @bodies_results =
-        Body.where("LOWER(name) LIKE ?", "%#{params[:f]}%".downcase)
-        .as_json.map{ |json| json.merge("url" => manage_body_path(json["id"])) }
+      group = Hash.new("Other")
+      group["Star"] = "Stars"
+      group["Planet"] = "Planets"
+      group["Minor Planet"] = "Planets"
+      group["Moon"] = "Moon"
+      group["Spacecraft"] = "Spacecraft"
+
+      search = Body.where("LOWER(name) LIKE ?", "%#{params[:f]}%".downcase)
+      @bodies_results = search.map do |body|
+        body.as_json.merge(
+          "url" => manage_body_path(body.id),
+          "group" => group[body.classification]
+        )
+      end
     end
   end
 end
